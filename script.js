278d87a11a0a73f86dcb893d92b8afaed347371d
marikadicarlo <mdicarlo19@yahoo.com>
2022-01-03 15:08:09 -0500

    js edits

--- script.js
+++ script.js
@@ -2,25 +2,65 @@ $('#search-button').on('click', function(event){
     var userInput = $('#city-input').val();
     event.preventDefault();
 
+    localStorage.setItem("city", "name");
+    document.getElementById("city-input").innerHTML = localStorage.getItem("city");
+    console.log(userInput);
+
     cityHistory.push(userInput);
     weather(userInput);
     renderCities();
+
+    $('#forecast-weather').empty();
+    $('#current-icon').empty();
+    $('#city-input').val("");
+
 });
 
+// Fetch Current Weather
 var weather = function(userInput) {
     fetch("https://api.openweathermap.org/data/2.5/weather?q=" + userInput + "&appid=293af217c8a05dd02fb63f4741d095dd")
-    .then(response => response.json())
-    .then(data => {
-        var nameValue = data['name'];
-
-
-        
+    .then((response) => {
+        if (!response.ok) throw new Error(response.statusText);
+            return response.json();
     })
-
-
+    .then(data => {
+        console.log(data);
+        displayWeather(data);
+    })
+    .catch(console.error);
 }
 
+function displayWeather(data) {
+    var temperature = data['main']['temp'];
+    var date = moment().format('MM/DD/YYYY');
+    var humidity = data['main']['humidity'];
+    var wind = data['wind'];
+    var latitude = data['coord']['lat'];
+    var longitude = data['coord']['lon'];
 
+    $("#city-name").text("City Name: " + data.name);
+    $("#current-temp").text("Temperature: " + data.main.temp + "°F");
+    $("#current-humidity").text("Humidity: " + data.main.humidity + "%");
+    $("#current-wind").text("Wind: " + data.wind.speed + "MPH");
+}
+
+// Fetch 5-Day Weather
+var fiveDayWeather = function(userInput) {
+    fetch("https://api.openweathermap.org/data/2.5/forecast/daily?q=" + userInput + "&appid=293af217c8a05dd02fb63f4741d095dd")
+    .then((response) => {
+        if (!response.ok) throw new Error(response.statusText);
+            return response.json();
+    }).then(data => {
+        console.log(data);
+        displayFiveDayWeather(data)
+    }).catch(console.error);
+}
+
+// function displayFiveDayWeather(data) {
+//     var temp = data['main']['temp'];
+
+//     $('#forecast-temp1').text("Temp: " + data.main.temp + "°F");
+// };
 
 
 var cityHistory = [];
